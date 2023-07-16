caa00687244ea2a7e689e8088cf9256a07648936
Nada Feteiha <nada.feteiha@gmail.com>
2023-07-16 12:37:03 -0700

    update blurred

--- src/main/kotlin/composables/BlurredColumn.kt
+++ src/main/kotlin/composables/BlurredColumn.kt
@@ -7,22 +7,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import ui.theme.grey
 
 @Composable
 fun BlurredCard(
     modifier: Modifier = Modifier,
-    content: @Composable () -> Unit
+    content: @Composable (modifier: Modifier) -> Unit
 ) {
     Box(
         modifier = modifier.clip(shape = RoundedCornerShape(24.dp))
     ) {
-        content()
+        content(
+            modifier = Modifier.background(
+                color = grey.copy(alpha = 0.4f),
+                shape = RoundedCornerShape(24.dp)
+            )
+        )
 
         Image(
             painter = painterResource("bg.svg"),
