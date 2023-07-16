b28460d34210eb0cb3fe70f2412772d434aa169a
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-07-16 19:04:32 +0300

    add compass ui

---
+++ src/main/kotlin/composables/Compass.kt
@@ -0,0 +1,37 @@
+package composables
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Compass(
+    state: CompassUiState
+){
+    Box(modifier = Modifier.size(192.dp), contentAlignment = Alignment.Center) {
+        Icon(
+            painter = painterResource("arrow.svg"),
+            contentDescription = "JetBrains",
+            modifier = Modifier
+                .size(192.dp)
+                .rotate(-290f)
+        )
+        Icon(
+            painter = painterResource("compass.svg"),
+            contentDescription = "JetBrains",
+            modifier = Modifier
+                .size(192.dp)
+        )
+    }
+}
+
+data class CompassUiState(
+    val windKph : Float = 0f,
+    val windDegree : Float = 0f
+)
