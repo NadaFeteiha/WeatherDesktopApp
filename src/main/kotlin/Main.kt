65578fd8774c312ae0671e11638e290f8c5fbf43
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-07-16 19:29:51 +0300

    delete composable preview

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,13 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import composables.Compass
-import composables.CompassUiState
+import data.remote.WeatherServiceImpl
+import kotlinx.coroutines.launch
 
 @Composable
 @Preview
@@ -15,16 +17,13 @@ fun App() {
     val coroutine = rememberCoroutineScope()
 
     MaterialTheme {
-
-        Compass(CompassUiState(windDegree = -45f, windKph = 10f))
-
-//        Button(onClick = {
-//            coroutine.launch {
-//                print(WeatherServiceImpl().getWeatherByCityName("London").location?.name)
-//            }
-//        }) {
-//            Text(text)
-//        }
+        Button(onClick = {
+            coroutine.launch {
+                print(WeatherServiceImpl().getWeatherByCityName("London").location?.name)
+            }
+        }) {
+            Text(text)
+        }
     }
 }
 
