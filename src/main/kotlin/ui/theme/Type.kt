2d672c505bee2b94ccbc145ceeaec85c25bf2743
Nada Feteiha <nada.feteiha@gmail.com>
2023-07-16 09:02:29 -0700

    add theme

---
+++ src/main/kotlin/ui/theme/Type.kt
@@ -0,0 +1,17 @@
+package ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val WeatherTypography = Typography(
+    h1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        color = Color.White
+    )
+)
