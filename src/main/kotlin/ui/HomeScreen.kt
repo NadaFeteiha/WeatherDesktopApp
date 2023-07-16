5bd89858678842b573ca894324307de2a6850b7c
Nada Feteiha <nada.feteiha@gmail.com>
2023-07-16 09:22:30 -0700

    add scroll

--- src/main/kotlin/ui/HomeScreen.kt
+++ src/main/kotlin/ui/HomeScreen.kt
@@ -1,27 +1,24 @@
 package ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.rememberScrollableState
-import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.WindowState
 import composable.HourlyForecastItem
-import composable.ProgressBar
-import ui.theme.Black
+import kotlinx.coroutines.launch
 import viewModel.ForecastHour
 import viewModel.HomeInteractionListener
 import viewModel.HomeUIState
@@ -34,12 +31,8 @@ fun HomeScreen(
     listener: HomeInteractionListener
 ) {
     Column(modifier = modifier.fillMaxSize().width(windowState.size.width)) {
-        Button(onClick = listener::getData) {
-            Text("Get Data")
-        }
 
         Text("Width = ${windowState.size.width}")
-
 
         HourlyForecast(
             forecastHourly = state.forecastHourly,
@@ -63,8 +56,17 @@ fun HourlyForecast(
             style = MaterialTheme.typography.h1
         )
 
+        val scrollState = rememberLazyListState()
+        val coroutineScope = rememberCoroutineScope()
         LazyRow(
-            modifier = Modifier.fillMaxWidth(),
+            state = scrollState,
+            modifier = Modifier.fillMaxWidth().draggable(
+                orientation = Orientation.Horizontal,
+                state = rememberDraggableState { delta ->
+                    coroutineScope.launch {
+                        scrollState.scrollBy(-delta)
+                    }
+                }),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(24.dp),
             contentPadding = PaddingValues(horizontal = 16.dp)
