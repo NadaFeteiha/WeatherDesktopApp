22a42b52941ee8fefcf26872a0599e2411db6d64
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-07-19 23:33:09 +0300

    fix BlurredCard

--- src/main/kotlin/ui/composables/BlurredCard.kt
+++ src/main/kotlin/ui/composables/BlurredCard.kt
@@ -1,37 +1,62 @@
 package ui.composables
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.*
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import ui.theme.*
+import ui.theme.Black
+import ui.theme.Blue
+import ui.theme.Pink
 
 @Composable
 fun BlurredCard(
     modifier: Modifier = Modifier,
+    color: Color = Color.DarkGray,
+    blurBackground: (@Composable () -> Unit)? = null,
     content: @Composable () -> Unit
 ) {
-    val gradient = Brush.horizontalGradient(
+    val gradient = Brush.linearGradient(
         listOf(
             Black.copy(alpha = 0.1f),
-            Yellow.copy(alpha = 0.2f),
+            Pink.copy(alpha = 0.2f),
             Blue.copy(alpha = 0.2f),
-            Orange.copy(alpha = 0.2f),
             Black.copy(alpha = 0.1f),
-        )
+        ),
     )
 
     Box(
         modifier = modifier
-            .clip(shape = RoundedCornerShape(24.dp))
-            .background(gradient),
+            .clip(shape = RoundedCornerShape(24.dp)),
         contentAlignment = Alignment.Center
     ) {
+        if (blurBackground != null) {
+            blurBackground()
+        } else {
+            Box(
+                modifier = Modifier
+                    .blur(100.dp, edgeTreatment = BlurredEdgeTreatment.Unbounded)
+                    .matchParentSize()
+                    .drawBehind {
+                        drawCircle(
+                            brush = gradient,
+                            radius = size.width * 0.5f,
+                        )
+                    }
+            )
+        }
+        Box(
+            modifier = Modifier
+                .matchParentSize()
+                .blur(24.dp, edgeTreatment = BlurredEdgeTreatment.Unbounded)
+                .alpha(0.05f)
+                .background(color = color)
+        )
         content()
     }
 }
