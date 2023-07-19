0e2b6587d4cf23059907744610f92b67916bbb34
Nada Feteiha <nada.feteiha@gmail.com>
2023-07-19 07:56:22 -0700

    remove unnecessary call

--- src/main/kotlin/viewModel/HomeViewModel.kt
+++ src/main/kotlin/viewModel/HomeViewModel.kt
@@ -8,8 +8,6 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
-import java.text.SimpleDateFormat
-import java.util.*
 
 class HomeViewModel(private val service: WeatherService) : HomeInteractionListener, KoinComponent {
 
@@ -19,7 +17,6 @@ class HomeViewModel(private val service: WeatherService) : HomeInteractionListen
     init {
         CoroutineScope(Dispatchers.IO).launch {
             _uiState.emit(service.getWeatherByCityName("lynnwood").toUIState())
-            formatDate()
         }
     }
 
@@ -42,18 +39,5 @@ class HomeViewModel(private val service: WeatherService) : HomeInteractionListen
             _uiState.emit(service.getWeatherByCityName(city).toUIState())
         }
     }
-
-    private fun formatDate() {
-        val inputFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
-        val outputFormat = SimpleDateFormat("dd MMMM, yyyy h:mm a", Locale.getDefault())
-
-        CoroutineScope(Dispatchers.IO).launch {
-            val inputDate = service.getWeatherByCityName(city = "lynnwood").location?.localtime
-            val parsedDate = inputFormat.parse(inputDate)
-            val outputDate = outputFormat.format(parsedDate)
-            _uiState.update { it.copy(date = outputDate) }
-        }
-    }
-
 
 }
