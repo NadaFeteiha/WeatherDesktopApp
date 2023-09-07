2e5ed9dd548ef97a97c990f2f75450c46d8b8f73
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-09-07 21:48:07 +0300

    Update build.gradle.kts and .gitignore files, add libs.versions.toml
    Updated dependencies and plugin versions in build.gradle.kts, making project use updated dependencies and removed hardcoded versions. This enables easy updates of library versions in the future.
    
    Simplified .gitignore to only ignore everything inside .idea directory, removing the need to manually specify individual files within .idea.
    
    Added libs.versions.toml file to centralize the versioning of dependencies, plugins, and libraries, improving maintainability and readability. This also supports type safe project accessors.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
+@file:Suppress("DSL_SCOPE_VIOLATION")
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+
 plugins {
-    kotlin("jvm") version "1.8.20"
-    id("org.jetbrains.compose") version "1.4.0"
+    kotlin("jvm") version "1.9.0"
     kotlin("plugin.serialization") version "1.8.20"
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kotlinKsp)
 }
 
 group = "me.nadafeteiha"
@@ -19,24 +22,27 @@ repositories {
 dependencies {
     implementation(compose.desktop.currentOs)
 
-    val ktor_version = "2.3.1"
-    implementation("io.ktor:ktor-client-core:$ktor_version")
-    implementation("io.ktor:ktor-client-cio:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
-    implementation("io.ktor:ktor-client-logging:$ktor_version")
-    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+//    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("org.slf4j:slf4j-simple:1.7.26")
 
+    api(libs.ktor.client.core)
+    api(libs.ktor.client.cio)
+    api(libs.ktor.json.serialization)
+    api(libs.kotlin.serialization)
+    api(libs.ktor.content.negotiation)
+    api(libs.ktor.logging)
+    api(libs.ktor.gson)
+
     //coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation(libs.kotlin.coroutines)
 
     //koin
-    implementation("io.insert-koin:koin-core:3.4.2")
-
-    implementation("io.insert-koin:koin-annotations:1.0.0")
+    api(libs.koin.core)
+    implementation(libs.koin.annotations)
+    implementation(libs.koin.ksp)
 
     //image
-    api("io.github.qdsfdhvh:image-loader:1.2.8")
+    api(libs.compose.image.loader)
 
 }
 
