ddcea0662013703d252da3352a1432aa7c81951c
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-09-08 16:09:05 +0300

    Refactor WeatherServiceImpl to improve readability and efficiency

--- src/main/kotlin/data/remote/WeatherServiceImpl.kt
+++ src/main/kotlin/data/remote/WeatherServiceImpl.kt
@@ -11,25 +11,25 @@ import io.ktor.client.statement.*
 import io.ktor.util.*
 import org.koin.core.component.KoinComponent
 
-class WeatherServiceImpl(private val client: HttpClient) : WeatherService, KoinComponent {
+class WeatherServiceImpl(private val client: HttpClient, private val attributes: Attributes) : WeatherService,
+    KoinComponent {
 
     override suspend fun getWeatherByCityName(city: String, numDays: Int): Weather =
-        tryToExecute<Weather>(APIS.WEATHER_API, "forecast.json?q=${city}&days=$numDays") { get(it) }
+        tryToExecute<Weather>(APIS.WEATHER_API) { get("forecast.json?q=${city}&days=$numDays") }
 
     override suspend fun getLocation(): LocationFromIP =
-        tryToExecute<LocationFromIP>(APIS.LOCATION_API, "check") { get(it) }
+        tryToExecute<LocationFromIP>(APIS.LOCATION_API) { get("check") }
 
     override suspend fun searchWeatherByCityName(city: String): List<SearchItem> =
-        tryToExecute<List<SearchItem>>(APIS.WEATHER_API, "search.json?q=${city}") { get(it) }
+        tryToExecute<List<SearchItem>>(APIS.WEATHER_API) { get("search.json?q=${city}") }
 
     private suspend inline fun <reified T> tryToExecute(
         api: APIS,
-        urlString: String,
-        method: HttpClient.(urlString: String) -> HttpResponse
+        method: HttpClient.() -> HttpResponse
     ): T {
         try {
-            WeatherServiceClient.clientAttributes.put(AttributeKey("API"), api.value)
-            val response = WeatherServiceClient.client.method(urlString)
+            attributes.put(AttributeKey("API"), api.value)
+            val response = client.method()
             return response.body<T>()
         } catch (e: Throwable) {
             throw e
